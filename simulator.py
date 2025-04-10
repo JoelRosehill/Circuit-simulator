cbb03748d37ab9ae4f953ee664abfb7df0a722e8
A242490JR <A242490JR@rvt.lv>
2025-04-10 11:39:23 +0300

    Updated simulator.py

--- simulator.py
+++ simulator.py
@@ -1,3 +1,13 @@
 import screen_size_check
+import os
 
-screen_size_check.lenght_check()
+screen_lenght = screen_size_check.width_check()
+screen_height = screen_size_check.height_check()
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print("╔════════════════════════╗")
+print("║ Circuit Live Simulator ║")
+print("╚════════════════════════╝")
+
+print('Your screen size:', str(screen_lenght) + 'x' + str(screen_height), 'symbol characters')
