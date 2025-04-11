b88ecd5aadeb438a3d2528b1dfacd708977c82bd
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-04-11 14:22:47 +0300

    Added more functions, testing database check in simullator.py

--- screen_ctrl/display_control_panel.py
+++ screen_ctrl/display_control_panel.py
@@ -2,7 +2,7 @@ import re
 
 def extract_numbers_from_line(line_number):
     try:
-        with open('db.txt', 'r') as file:
+        with open('../database/db.txt', 'r') as file:
             lines = file.readlines()
             if line_number <= 0 or line_number > len(lines):
                 raise ValueError("Line number out of range.")
